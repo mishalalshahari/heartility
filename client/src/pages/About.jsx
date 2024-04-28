870a08cba95a4191b9803e7657d0dcf8a88c8331
mishalalshahari <mishalalshahari@gmail.com>
2024-04-29 01:10:52 +0530

    added repo link

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
           We are non profit group of developers who are passionate about helping people. We will analyze, predict the result whether the patient has heart disease or normal, i.e. Heart disease prediction using Machine Learning.
           <br />
           <br />
-          Feel free to contribute to our project!  <span className="bg-green-700 text-white rounded-3xl p-3 hover:opacity-90 hover:underline"><a href="">Heartility - GitHub</a></span>
+          Feel free to contribute to our project!  <span className="bg-green-700 text-white rounded-3xl p-3 hover:opacity-90 hover:underline"><a href="https://github.com/mishalalshahari/heartility">Heartility - GitHub</a></span>
       </div>
       <div className='flex'>
         <div>
