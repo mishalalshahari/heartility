3416ce15f8c7a08c8e25845594c076d1b0c13e90
mishalalshahari <mishalalshahari@gmail.com>
2024-04-29 14:14:28 +0530

    added deployment build

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGO)
     .catch((err) => {
         console.log('Error: ', err);
     });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -27,6 +30,12 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
